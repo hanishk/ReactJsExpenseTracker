4b4ef788012991e4d25d67eeeaf2ee26a4aeee81
Hanish Kumar <hanishk616@gmail.com>
2022-04-10 00:43:04 +0530

    More Css,Multiple Components & Props

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,5 +1,17 @@
-function ExpenseItems() {
-    return <h2>This is Custom Component</h2>;
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItems.css";
+
+function ExpenseItems(props) {
+
+  return (
+    <div className="expense-item">
+    <ExpenseDate date = {props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+      </div>
+      <div className="expense-item__price">${props.amount}</div>
+    </div>
+  );
 }
 
 export default ExpenseItems;
