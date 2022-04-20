8ddf770d86918ecf27c5272394ad5ff4a9dd5217
Hanish Kumar <hanishk616@gmail.com>
2022-04-21 00:21:31 +0530

    Update Folder Structure

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -1,6 +1,6 @@
 import ExpenseItems from "./ExpenseItems";
 import './Expense.css';
-import Card from "./Card";
+import Card from "../UI/Card";
 
 function Expense(props){
 return <Card className="expenses">
